d7e3ea94b2d081aff5a3f4ade08dc1b2f26fa5fb
ShakilAhmed007 <sakilhossain01969@gmail.com>
2021-09-15 03:00:45 +0600

    ✨ Add: tailwind added

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className="w-full min-h-screen bg-gray-900">
+        <section className="absolute transform -translate-x-1/2 -translate-y-1/2 bg-gray-400 h-96 w-80 rounded-2xl rotate-3 top-1/2 left-1/2">
+        </section>
+        <section className="absolute p-6 space-y-6 duration-300 transform -translate-x-1/2 -translate-y-1/2 bg-gray-100 h-96 w-80 rounded-2xl rotate-3 hover:rotate-0 top-1/2 left-1/2">
+          <div className="flex justify-end">
+            <div className="w-4 h-4 bg-gray-900 rounded-full" />
+          </div>
+          <header className="text-xl font-extrabold text-center text-gray-600">Wellcome To</header>
+          <div>
+            <p className="text-5xl font-extrabold text-center text-gray-900">React <br />+<br />(Tailwind)</p>
+            <p className="text-4xl font-extrabold text-center" style={{ "color": "#FE5401" }}>Starter!</p>
+          </div>
+
+        </section>
+      </main>
+
     </div>
   );
 }
